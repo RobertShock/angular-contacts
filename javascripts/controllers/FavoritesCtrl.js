99f0f3ae105d2b9f14226eeb93b5cfbafa6d2a1f
Robbie Shock <rob.f.shock@gmail.com>
2017-11-20 19:21:43 -0600

    killed favorites tab

--- javascripts/controllers/FavoritesCtrl.js
+++ javascripts/controllers/FavoritesCtrl.js
@@ -3,21 +3,21 @@
 app.controller("FavoritesCtrl", function($location, $rootScope, $scope, ContactService){
 	$scope.contacts = [];
 
-	const getContacts = () => {
-		ContactService.getFavoriteContacts($rootScope.uid).then((results) =>{
-			$scope.contacts = results;
-		}).catch((err) =>{
-			console.log("error in getFavoriteContacts", err);
-		});
-	};
+	// const getContacts = () => {
+	// 	ContactService.getFavoriteContacts($rootScope.uid).then((results) =>{
+	// 		$scope.contacts = results;
+	// 	}).catch((err) =>{
+	// 		console.log("error in getFavoriteContacts", err);
+	// 	});
+	// };
 
-	getContacts();
+	// getContacts();
 
-	$scope.deleteContact = (contactId) => {
-		ContactService.deletecontact(contactId).then((result) => {
-			getContacts();
-		}).catch((err) =>{
-			console.log("error in deleteContact", err);
-		});
-	};
+	// $scope.deleteContact = (contactId) => {
+	// 	ContactService.deletecontact(contactId).then((result) => {
+	// 		getContacts();
+	// 	}).catch((err) =>{
+	// 		console.log("error in deleteContact", err);
+	// 	});
+	// };
 });
