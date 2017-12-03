56a81f07990973bfff6d1e5b37501292a1ebf883
Robbie Shock <rob.f.shock@gmail.com>
2017-12-03 13:41:03 -0600

    bug fixes

--- javascripts/controllers/ContactDetailCtrl.js
+++ javascripts/controllers/ContactDetailCtrl.js
@@ -2,7 +2,7 @@
 
 app.controller('ContactDetailCtrl', function($location, $routeParams, $scope, ContactService){
 
-	$scope.contact = {};
+	// $scope.contact = {};
 
 	const getContacts = () => {
 		ContactService.getSingleContact($routeParams.id).then((results) => {
@@ -12,16 +12,24 @@ app.controller('ContactDetailCtrl', function($location, $routeParams, $scope, Co
    		});	
 	};
 
+	// const getContact = () => {
+    //     ContactService.getSingleContact($routeParams.id).then((results) => {
+    //         $scope.contact = results.data;
+    //     }).catch((err) => {
+    //         console.log('error in getContact', err);
+    //     });
+    // };
+
 	$scope.switchFavorite = (contact) => {
 		contact.favorite = contact.favorite ? false: true;
 		let favoriteContact = ContactService.createContactObj (contact);
 		ContactService.updateContact(favoriteContact, contact.id).then((result) => {
-			getContacts();
+			getContact();
 		}).catch((err) => {
 			console.log('error in favoriteContact', err);
 		});
 	};
-
+	
 	$scope.editContact = (contactId) => {
 		$location.path(`/contacts/edit/${contactId}`);
 	};
